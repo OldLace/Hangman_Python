8d64e086794677b9bd5364992661eca91bd2259e
Paul Gelot <pgelot@gmail.com>
2018-06-21 22:02:40 -0400

    first commit

---
+++ hangman.py
@@ -0,0 +1,19 @@
+# Using the Exercise Branches and Functions, create a simple puzzle game which starts with
+# asking user a question. For every question, you should either give user the option to select from
+# or let user enter the answer. For each user answer, your game should take the next step
+# accordingly using conditions.
+# 1. Your game should ask user for input at least five ​times
+# 2. Your code should have functions (basically for everything)
+
+answer_letters = ['B','L','U','E']
+
+possible_choices = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','N','O','O'
+
+def play(answer):
+    choice = input('Pick a letter: ')
+
+
+
+
+
+# randint
