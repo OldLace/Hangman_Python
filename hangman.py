a0d4f5a1631d5132eeac1a45d5d552cf4938a024
Paul Gelot <pgelot@gmail.com>
2018-07-10 23:10:43 -0400

    rewrote answer detection

--- hangman.py
+++ hangman.py
@@ -4,127 +4,52 @@ import random #used later to generate random number for result index
 import sys #used to quit the program when win or loss condition is met
 import time #used  later for the time delays
 
+
 def play_game():
-    answer_letters = []
-    correct_guesses =[]
+    word = 'millenials'
+    right_counter = 0
+    wrong_counter = 0
 
-    status = 5 #Number of guesses player starts game with
-    win_state = 0
-    right = 0  #Variable for how many correct guesses the player has made
+    previous_choices = []
 
-    the_double = '1'
-    double_flag = 0
-    doubles = []
+    def create_underscore_word(word):
+        new_word_underscore = []
+        for i in  range(len(word)):
+            new_word_underscore.append('_')
+        return new_word_underscore
+            
+    word_underscore = create_underscore_word(word)
 
-    choice_guesses = []
-    
-    #All the possible word answers for the game are here in the form of a list 
-    # result = ['dog', 'bird', 'snake','bear', 'blue','black', 'white', 'orange', 'computer', 'virtual','peanut','netflix']
-    result = ['breally','poggy']
+    print("This is your word: ", ' '.join(word_underscore))
+    while True: 
+        choice = input("Choose a letter: ")
 
-       #Used to generate a random number from 0 to 11; the possible indices of the result list
-    word_idx = random.randint(-1,1)
-
-    for i in range(len(result[word_idx]) -1):
-        if result[word_idx][i] == result[word_idx][i+1]:
-            doubles.append(result[word_idx][i])
-    double_flag = len(doubles)
-
-    for i in range(len(result[word_idx]) -1):
-        if result[word_idx][i] == result[word_idx][i+1]:
-            the_double = result[word_idx][i]
-    # print(dupe_list)
-
- 
-
-    #Needed to get the length of the result element in order to populate it with as many * as the length of the result 
-    length_element = [len(i) for i in result[word_idx]]
-    correct_guesses = ['_'] * len(result[word_idx])
-
-    print('*** Welcome to the Guessing Game!! ***')
-    # time.sleep(2) #Pause for 2 seconds for dramatic effect
-    print('---------------How to Play:---------------')
-    print('Guess which letters are in the word!')
-    # time.sleep(1)
-    print('You begin with', status,'guesses.')
-    # time.sleep(1)
-    print("NOTE: You can type 'status' to see how many chances you have left.")
-    # time.sleep(2)
-    print("Run out of guesses and it's...")
-
-    over = ['GAME','OVER','FOR','YOU!','---------------------------------------'] 
-    # A fun loop to add some character and flair to this game
-    for i in over:
-        print(i)
-        # time.sleep(1)
-    print("This is your word:",' '.join(correct_guesses))
-
-    
-    for i in result[word_idx]:  #loops through the word at the randomly chosen index
-        answer_letters.append(i)   #puts the letters of the word to the answers_letters list
-    while win_state == 0:
-        if right == len(answer_letters):  #Detects if the number of right guesses is equal to the length of the word
-            print('You win!!!!')
-            restart_game()
-        choice = str(input('Pick a letter: '))
-        if choice in doubles:
-            double_flag = double_flag + 1
-            doubles.remove(choice)
-            # choice not in correct_guesses:
-            idx = answer_letters.index(choice)
-            correct_guesses.pop(idx + 1)
-            correct_guesses.pop(idx)
-            correct_guesses.insert(idx, choice)
-            correct_guesses.insert(idx + 1, choice)
-            print(' '.join(correct_guesses))
-            right = right + 2
-            # double_flag = double_flag + 1
-            # doubles.remove(choice)
-        elif choice in correct_guesses:
+        temp = right_counter + 0
+        #check previous choices here
+        if len(choice) > 1:
+            print("Invalid choice. Only 1 letter per turn")
+            continue
+        if choice in previous_choices:
             print("Letter already used")
-            status = status -1 
-            if status <= 0:
-                print('***** Game Over *****')
-                restart_game()
-            # else: 
-            #     double_flag = double_flag + 1
-            #     doubles.remove(choice)
-            #     # choice not in correct_guesses:
-            #     idx = answer_letters.index(choice)
-            #     correct_guesses.pop(idx + 1)
-            #     correct_guesses.pop(idx)
-            #     correct_guesses.insert(idx, choice)
-            #     correct_guesses.insert(idx + 1, choice)
-            #     print(' '.join(correct_guesses))
-            #     right = right + 2
-
-
-        # if choice in correct_guesses:  #This loop is used to detect duplicate guesses
-        #     print("Letter already used")
-        #     status = status -1 
-        #     if status <= 0:
-        #         print('***** Game Over *****')
-        #         restart_game()
-                
-        elif choice in answer_letters:
-            print('correct!')
-            idx = answer_letters.index(choice)
-            correct_guesses.pop(idx)
-            correct_guesses.insert(idx,choice) #inserts correct choice into list at the appropriate index
-            print(' '.join(correct_guesses))
-            right = right + 1
-       
-        elif choice == 'status':
-            if status > 1:
-                print(status, "guesses remaining...")
-            else:
-                print(status, "guess remaining... no pressure! (._.) ")    
-        else:
-            print('Incorrect choice!')
-            status = status -1 
-            if status <= 0:
-                print('***** Game Over! Better Luck Next Time... *****') 
-                restart_game()
+            continue
+        previous_choices.append(choice)
+        for i in range(len(word)):
+            word_letter = word[i]
+            if choice == word_letter:
+                right_counter += 1
+                word_underscore[i] = word[i]
+        print(' '.join(word_underscore))
+        if temp == right_counter:
+            wrong_counter += 1
+            print('Incorrect guess')
+        if right_counter == len(word):
+            print("You win!!")
+            restart_game()
+            return
+        elif wrong_counter == 5:
+            print('You lose')
+            restart_game()
+            return
 
 def restart_game(): #function to restart game based on user input
     restart = input('Play again? ')
@@ -142,4 +67,4 @@ def restart_game(): #function to restart game based on user input
         print('Invalid choice. Play Again? Yes or No?')
         restart_game()
 
-play_game()
+play_game() 
