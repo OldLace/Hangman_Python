1e1993085cdc64455a62fe638f0c7a671353bdfd
Paul Gelot <pgelot@gmail.com>
2018-07-12 20:55:58 -0400

    game recognizes e with acute accent

--- hangman.py
+++ hangman.py
@@ -11,6 +11,7 @@ def play_game():
     #Used to generate a random number from 0 to 17; the possible indices of the result list
     random_index = random.randint(-1,17)
 
+    # word = 'résumé-resume'
     word = result[random_index]
     right_counter = 0
     wrong_counter = 0
@@ -19,16 +20,23 @@ def play_game():
 
     def create_underscore_word(word):
         new_word_underscore = []
+        special_char = 0
         for i in  range(len(word)):
             new_word_underscore.append('_')
+        # for i in range(len(word)):
+            # if word[i] == '-':
+            #     special_char = 1
+            #     new_word_underscore[i] = word[i]  
         return new_word_underscore
             
     word_underscore = create_underscore_word(word)
-
-    print("This is your word: ", ' '.join(word_underscore))
+    # word_underscore = create_underscore_word[0]
+    print("This is your word: ") 
+    print(' '.join(word_underscore))
+    
     while True: 
         choice = input("Choose a letter: ")
-
+        
         temp = right_counter + 0
         #check previous choices here
         if len(choice) > 1:
@@ -40,8 +48,11 @@ def play_game():
         previous_choices.append(choice)
         for i in range(len(word)):
             word_letter = word[i]
+            if word_letter == 'é':
+                word_letter = 'e'
             if choice == word_letter:
                 right_counter += 1
+                print("Correct!")
                 word_underscore[i] = word[i]
         print(' '.join(word_underscore))
         if temp == right_counter:
